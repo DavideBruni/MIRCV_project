5a9167f3a6503fea97c2df5bd2e291a4f358722d
DavideBruni <davidebruni.005@outlook.it>
2024-01-23 17:34:55 +0100

    Minor bug fix

--- search_engine/src/main/java/unipi/aide/mircv/queryProcessor/QueryProcessorMain.java
+++ search_engine/src/main/java/unipi/aide/mircv/queryProcessor/QueryProcessorMain.java
@@ -95,7 +95,7 @@ public class QueryProcessorMain {
             filename += "parsed.txt";
         else
             filename += "not_parsed.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader("msmarco-test2019-queries.tsv"));
+        try (BufferedReader br = new BufferedReader(new FileReader("msmarco-test2020-queries.tsv"));
              BufferedWriter resultStream = new BufferedWriter(new FileWriter(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
